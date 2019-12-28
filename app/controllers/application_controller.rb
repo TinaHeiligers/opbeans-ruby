6e5ac984eea18011edcff1387647df63ff1a8fcb
Christiane Heiligers <christiane.heiligers@elastic.co>
2019-12-28 16:34:17 -0700

    Removes rum injection from application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,6 @@ class ApplicationController < ActionController::Base
 
   User = Struct.new(:id, :email, :username)
 
-  before_action do
-    ElasticAPM.set_label :company, 'opbeans'
-    # ElasticAPM.set_user User.new(3229, 'coffeelover@example.com', 'coffeelover')
-  end
-
   INDEX_FILE =
     File.read(Rails.root.join('frontend', 'build', 'index.html'))
 
@@ -21,25 +16,4 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def inject_rum_config(html)
-    html
-      .gsub(
-        '<head>',
-        "<head><script>window.rumConfig = #{rum_config.to_json}</script>"
-      ).gsub(
-        '<script type="text/javascript" src="/rum-config.js"></script>',
-        ''
-      )
-  end
-
-  def rum_config
-    {
-      serviceName: ENV['ELASTIC_APM_JS_SERVICE_NAME'],
-      serviceVersion: ENV['ELASTIC_APM_JS_SERVICE_VERSION'],
-      serverUrl: ENV['ELASTIC_APM_JS_SERVER_URL'],
-      pageLoadTraceId: ElasticAPM.current_transaction&.trace_id,
-      pageLoadSpanId: ElasticAPM.current_transaction&.ensure_parent_id,
-      pageLoadSampled: ElasticAPM.current_transaction&.sampled?
-    }
-  end
 end
